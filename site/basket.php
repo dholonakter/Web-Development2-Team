bf77ea042f6bd0af68553259d0f211bf4abce0f8
lpddat <lpddat@mail.com>
2018-10-26 22:21:24 +0200

    basket

--- site/basket.php
+++ site/basket.php
@@ -1,25 +1,80 @@
 <?php include "header.php"; ?>
 
 
-<h1>You have selected your items!!!</h1>
+<h1>Your selected items!!!</h1>
 
-<div class="container">
-    <div class="imagerow">
-        <div class="imagecolumn">
-            <figure>
-                <img class="image" src="images/product4.jpeg" width="300" height="200">
-                <figcaption>Clothes</figcaption>
-            </figure>
-            <figure>
-                <img class="image" src="images/product2.jpeg" width="300" height="200">
-                <figcaption>Accessories</figcaption>
-            </figure>
-            <figure>
-                <img class="image" src="images/product3.jpeg" width="300" height="200">
-                <figcaption>Electronic devices</figcaption>
-            </figure>
-        </div>
+<div class="basket">
+    <title>Shopping basket</title>
+<div class="basketitem">
+        <div class="product">
+    <img src="images/headphone.jpeg" width="200px" height="200px">
     </div>
+    <div class="description">
+    <span>Headphone</span><br>
+        <span>Black</span>
+    </div>
+   <div class="basketquantity">
+        <button class="basketbtn" type="button" name="button">
+        <img src="images/minus.jpg" width="20px" height="20px"></button>   
+        <input type="text" name="name" value="1">
+         <button class="basketbtn" type="button" name="button">
+        <img src="images/plus.jpg" width="20px" height="20px"></button>
+            </div>
+    <div class="totalprice">80EUR</div>
+    </div>
+    
+<div class="basketitem">
+    
+    <div class="product">
+    <img src="images/backpack.jpeg" width="200px" height="200px">
+    </div>
+    <div class="description">
+    <span>Backpack</span><br>
+        <span>Black</span>
+    </div>
+    <div class="basketquantity">
+        <button class="basketbtn" type="button" name="button">
+        <img src="images/minus.jpg" width="20px" height="20px"></button>   
+        <input type="text" name="name" value="1">
+         <button class="basketbtn" type="button" name="button">
+        <img src="images/plus.jpg" width="20px" height="20px"></button>
+            </div>
+    <div class="totalprice">50EUR</div>
+    </div>
+    
+    <div class="basketitem">
+    
+    <div class="product">
+    <img src="images/dress.png" width="200px" height="200px">
+    </div>
+    <div class="description">
+    <span>Long Dress</span><br>
+        <span>Black</span>
+    </div>
+    <div class="basketquantity">
+        <button class="basketbtn" type="button" name="button">
+        <img src="images/minus.jpg" width="20px" height="20px"></button>   
+        <input type="text" name="name" value="1">
+         <button class="basketbtn" type="button" name="button">
+        <img src="images/plus.jpg" width="20px" height="20px"></button>
+            </div>
+    <div class="totalprice">90EUR</div>
+    </div>
+    </div>
+    <div class="basketclearfix">
+
+            <div class="left">
+                <h2 class="subtotal">Subtotal: <span>220</span>€</h2>
+                
+                <h3 class="shipping">Shipping: <span>5.00</span>€</h3>
+            </div>
+
+            <div class="right">
+                <h1 class="total">Total: <span>225</span>€</h1>
+                <button class="checkoutbtn">Checkout</button>
+                
+            </div>
+        <script type="text/javascript" src="JS/script.js"></script>
 </div>
  
     <?php include "footer.php" ?>
