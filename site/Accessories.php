24bb4aa9189cb5f800b883240b282f5c05863d98
Dholon Akter <dholonakter721@gmail.com>
2018-10-27 11:52:04 +0200

    extra something

---
+++ site/Accessories.php
@@ -0,0 +1,88 @@
+ 
+<?php include "header.php"; ?>
+
+<div class="sidenav">
+  <a href="clothes.php">Clothes</a>
+  <a href="Accessories.php">Accessories</a>
+</div>
+
+
+ <div class="rowgalary">
+     
+
+
+<div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        49.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/SS860.JPEG">
+    <img src="images/SS860.JPEG"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone 
+        29.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div>
+         <div class="gallery">
+  <a target="_blank" href="images/RAM%20DDR3.PNG">
+    <img src="images/RAM%20DDR3.PNG"  width="600" height="400">
+  </a>
+  <div class="desc">RAM DDR3 <br>
+      39.95 EUR</div>
+       <button class="button" type="button">Add to cart</button> 
+</div>
+
+
+<div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        19.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div>
+    
+<div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        19.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div><div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        19.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div><div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        19.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div><div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        19.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div><div class="gallery">
+  <a target="_blank" href="images/headphone.jpeg">
+    <img src="images/headphone.jpeg"  width="600" height="400">
+  </a>
+  <div class="desc">Blutooth Headphone
+        19.95 EUR</div>
+    <button class="button" type="button">Add to cart</button> 
+</div>
+ </div>
+    <?php include "footer.php" ?>
+
